ea20d47bea42dfd354df3d80717eb661a3655a25
Ibeneme <ibenemeikenna96@gmail.com>
2024-12-04 16:02:18 +0100

    first commit

--- src/Components/Sections/AnimatedText/AnimatedText.tsx
+++ src/Components/Sections/AnimatedText/AnimatedText.tsx
@@ -1,38 +1,10 @@
-import React, { useEffect } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import "./animatedTextStyles.css"; // Import CSS file
 
-// Register ScrollTrigger plugin
-gsap.registerPlugin(ScrollTrigger);
+type Props = {}
 
-// Component
-const AnimatedText: React.FC = () => {
-  useEffect(() => {
-    // Animation logic
-    const textElements = gsap.utils.toArray(".lastsection-Text");
-    textElements.forEach((text: any) => {
-      gsap.to(text, {
-        backgroundSize: "100%",
-        ease: "none",
-        scrollTrigger: {
-          trigger: text,
-          start: "center 80%",
-          end: "center 20%",
-          scrub: true,
-        },
-      });
-    });
-  }, []);
-
+function AnimatedText({}: Props) {
   return (
-    <div className="lastsection-Container">
-      <h1 className="lastsection-Text">INTEGRITY AND PROFESSIONALISM,</h1>
-      <h1 className="lastsection-Text">RESPECT FOR ALL,</h1>
-      <h1 className="lastsection-Text">DIVERSITY & INCLUSIVENESS,</h1>
-      <h1 className="lastsection-Text">ZERO DISCRIMINATION.</h1>
-    </div>
-  );
-};
+    <div>AnimatedText</div>
+  )
+}
 
-export default AnimatedText;
+export default AnimatedText
